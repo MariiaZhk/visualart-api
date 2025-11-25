1651a3497ec2a2e47b8b08c986c5e593970f872c
Mariia <maria8zhuk@gmail.com>
2025-11-25 20:01:03 +0100

    Initial commit

---
+++ src/main/java/com/visualart/VisualartServiceApplication.java
@@ -0,0 +1,12 @@
+package com.visualart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualartServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisualartServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/visualart/VisualartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.visualart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualartServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+     
+    }
+}
