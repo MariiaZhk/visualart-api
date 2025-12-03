12009bdc3ac1f74f5973f2956d793d69104ddf19
Mariia Zhk <maria8zhuk@gmail.com>
2025-12-03 20:26:08 +0100

    Змінила шляхи контролерів на /api/artists та /api/artworks

--- src/main/java/com/visualart/controller/ArtistController.java
+++ src/main/java/com/visualart/controller/ArtistController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/artist")
+@RequestMapping("/api/artists")
 @RequiredArgsConstructor
 public class ArtistController {
 
